15e77dd309ad26b045ecad07b3928fb8e187f12d
Mark Webb <Testablekarma@users.noreply.github.com>
2020-11-05 14:42:49 +0000

    asd

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "UIComponents",
-            targets: ["UIComponents"]
+            targets: ["UIComponents", "NSSComponents"]
         )
     ],
     dependencies: [
