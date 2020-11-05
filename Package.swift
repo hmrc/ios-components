01ea4c65423ca70256cdb4322f1b8c1acb3ed26c
Mark Webb <Testablekarma@users.noreply.github.com>
2020-11-05 12:03:47 +0000

    Adds nss colours and target

--- Package.swift
+++ Package.swift
@@ -9,10 +9,8 @@ let package = Package(
     platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "UIComponents",
-            targets: ["UIComponents"]
-        ),
+        .library(name: "UIComponents", targets: ["UIComponents"]),
+        .library(name: "NSSComponents", targets: ["NSSComponents"])
     ],
     dependencies: [
         .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1"))
@@ -21,16 +19,26 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "UIComponents",
+            name: "ComponentsCommon",
             dependencies: ["SnapKit"],
+            path: "Sources/Shared",
             resources: [
-                .process("Assets"),
+                .process("Assets")
+             ]
+        ),
+        .target(
+            name: "UIComponents",
+            dependencies: ["ComponentsCommon"],
+            resources: [
                 .process("Colors")
             ]
         ),
-        .testTarget(
-            name: "UIComponentsTests",
-            dependencies: ["UIComponents"]
-        ),
+        .target(
+            name: "NSSComponents",
+            dependencies: ["ComponentsCommon"],
+            resources: [
+                .process("Colors")
+            ]
+        )
     ]
 )
