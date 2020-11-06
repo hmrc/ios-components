9506cdaf26833878a4746440148036c3ea8a3b37
Mark Webb <Testablekarma@users.noreply.github.com>
2020-11-06 09:26:19 +0000

    Renames targets

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "UIComponents", targets: ["UIComponents"]),
+        .library(name: "HMRCComponents", targets: ["HMRCComponents"]),
         .library(name: "NSSComponents", targets: ["NSSComponents"])
     ],
     dependencies: [
@@ -19,23 +19,22 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "ComponentsCommon",
+            name: "UIComponents",
             dependencies: ["SnapKit"],
-            path: "Sources/Shared",
             resources: [
                 .process("Assets")
              ]
         ),
         .target(
-            name: "UIComponents",
-            dependencies: ["ComponentsCommon"],
+            name: "HMRCComponents",
+            dependencies: ["UIComponents"],
             resources: [
                 .process("Colors")
             ]
         ),
         .target(
             name: "NSSComponents",
-            dependencies: ["ComponentsCommon"],
+            dependencies: ["UIComponents"],
             resources: [
                 .process("Colors")
             ]
