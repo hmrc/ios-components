cafc79776e457cbd297de390e55ebfee00c7ff27
Tendai Moffatt <38880662+tendai-moffatt@users.noreply.github.com>
2021-01-21 13:32:28 +0000

    [HMA-3887] Make ImageWithColour public

--- Sources/UIComponents/Helpers/UIImage+Color.swift
+++ Sources/UIComponents/Helpers/UIImage+Color.swift
@@ -16,7 +16,7 @@
 
 import UIKit
 
-extension UIImage {
+public extension UIImage {
     class func imageWithColor(color: UIColor) -> UIImage {
         let rect: CGRect = CGRect(x: 0, y: 0, width: 1, height: 1)
         UIGraphicsBeginImageContextWithOptions(CGSize(width: 1, height: 1), false, 0)
