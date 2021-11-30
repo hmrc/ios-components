340ff9321dddb2e4c21ef98e259c71a8391b3b8b
Chris Birch <3192947+chrisbirch@users.noreply.github.com>
2021-11-30 14:52:29 +0000

    [HMA-5191] - Dynamic colour fix

--- Sources/UIComponents/Organisms/InformationMessage/InformationMessage.swift
+++ Sources/UIComponents/Organisms/InformationMessage/InformationMessage.swift
@@ -72,8 +72,8 @@ extension Components.Organisms {
                 warningCard.iconImageView.tintColor = UIColor.Semantic.lightText
             case .warning:
                 warningParentCard.backgroundColor = UIColor.Named.yellow.colour
-                warningCard.bodyLabel.textColor = UIColor.Named.black.colour
-                warningCard.iconImageView.tintColor = UIColor.Named.black.colour
+                warningCard.bodyLabel.textColor = UIColor.black
+                warningCard.iconImageView.tintColor = UIColor.black
             case .notice:
                 warningParentCard.backgroundColor = UIColor.Semantic.darkText
                 warningCard.bodyLabel.textColor = UIColor.Semantic.lightText
@@ -97,15 +97,15 @@ extension Components.Organisms {
                     style: .primary(true, baseline: false),
                     string: ctaModel.message
                 )
+                
+                if let button = button as? HMRCButton {
+                    button.setBackgroundColor(.Semantic.whiteBackground, for: .normal)
+                    button.setBackgroundColor(.Semantic.secondaryButtonHighlightedBackground, for: .highlighted)
+                    
+                } else {
+                    fatalError("Something has gone wrong. Button should be HMRCButton type")
+                }
 
-                button.setBackgroundImage(
-                    UIImage.imageWithColor(color: UIColor.Semantic.whiteBackground),
-                    for: .normal
-                )
-                button.setBackgroundImage(
-                    UIImage.imageWithColor(color: UIColor.Semantic.secondaryButtonHighlightedBackground),
-                    for: .highlighted
-                )
                 button.setTitleColor(UIColor.Semantic.secondaryButtonText, for: .normal)
 
                 if let accessibilityIdentifier = ctaModel.accessibilityHint,
