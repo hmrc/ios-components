e837df344e146d0fdd36bd764db0c86fd726928c
Tendai Moffatt <38880662+tendai-moffatt@users.noreply.github.com>
2021-12-10 17:23:10 +0000

    ....

--- Sources/UIComponents/Molecules/TabBarView.swift
+++ Sources/UIComponents/Molecules/TabBarView.swift
@@ -85,7 +85,7 @@ extension Components.Molecules {
             get {
                 return segmentButtons.firstIndex(where: { $0.isSelected }) ?? 0
             }
-            set (oldValue, newValue) {
+            didSet (oldValue, newValue) {
                 if oldValue != newValue {
                     segmentButtons[oldValue].update(isSelected: false)
                     segmentButtons[newValue].update(isSelected: true)
