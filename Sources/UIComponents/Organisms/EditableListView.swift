24e7f765d2d112c1615a6056afbf8f86d71839a6
Tendai Moffatt <38880662+tendai-moffatt@users.noreply.github.com>
2022-12-20 09:39:20 +0000

    .

--- Sources/UIComponents/Organisms/EditableListView.swift
+++ Sources/UIComponents/Organisms/EditableListView.swift
@@ -208,7 +208,7 @@ extension Components.Organisms {
                 content.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -.spacer16),
                 editButton.rightAnchor.constraint(equalTo: rightAnchor, constant: -.spacer16),
                 editButton.centerYAnchor.constraint(equalTo: centerYAnchor),
-                editButton.heightAnchor.constraint(lessThanOrEqualTo: content.heightAnchor),
+//                editButton.heightAnchor.constraint(lessThanOrEqualTo: content.heightAnchor),
                 //editButton.heightAnchor.constraint(equalToConstant: 50),
 //                editButton.topAnchor.constraint(equalTo: topAnchor),
 //                editButton.bottomAnchor.constraint(equalTo: bottomAnchor),
